65478675a880f56049b4a06e1634f556aac6fba7
m3xan1k <dev.serzh@gmail.com>
2020-08-10 13:17:26 +0300

    admin and timezone

--- app/clients/admin.py
+++ app/clients/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from clients.models import Client
 
 
-admin.site.register(Client)
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', 'source_ip', 'destination_ip', 'connected',
+                    'confirmed', 'last_connection_time', 'unconfirmed_connections_count')
